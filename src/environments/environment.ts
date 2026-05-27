68a1e61e3af22b85be7d40ca5b921f46b89460a3
Ivan Nakatani <77029720+IvanNaka@users.noreply.github.com>
2026-05-26 21:31:35 -0300

    fix ports

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44384/api',
+  apiUrl: 'https://localhost:32768/api',
   googleClientId: '313428428387-bsd50stnkjjkibp21pcue7fvcr6vb0bl.apps.googleusercontent.com'
 };
